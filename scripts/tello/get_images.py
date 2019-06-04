4e4b448961c5f29caaa724f753de1865fc7713ab
moshes7 <moshes777@gmail.com>
2019-06-04 20:49:52 +0300

    get_images.py: add option of saving images
    camera_calibration.py: script for camera calibration
    
    265x265mm.png: checkerboard image used for camera calibration

--- scripts/tello/get_images.py
+++ scripts/tello/get_images.py
@@ -1,7 +1,14 @@
-from djitellopy import Tello
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import cv2
+import os
 import numpy as np
 import matplotlib.pyplot as plt
+from djitellopy import Tello
+
+# --- parameters
+save_images = True
+output_folder = r'C:\Users\Moshe\Sync\Projects\tello\images\camera1'
 
 # initializations
 tello = Tello()
@@ -19,16 +26,26 @@ plt.figure()
 img = np.zeros((720,960,3))
 h = plt.imshow(img)
 plt.show(block=False)
-iterations = 1000
+iterations = 300
 
 counter = 0
+frame_num = 0
 while counter < iterations:
     counter += 1
-    frame = cv2.cvtColor(frame_read.frame, cv2.COLOR_BGR2RGB)
+    frame = frame_read.frame
+    frame_rgb = cv2.cvtColor(frame_read.frame, cv2.COLOR_BGR2RGB)
     # frame = np.rot90(frame)
     # frame = np.flipud(frame)
     # plt.imshow(frame)
-    h.set_data(frame)
+
+    if save_images:
+        print('saving frame {}', frame_num)
+        img_name = os.path.join(output_folder, '{0:03d}.jpg'.format(frame_num))
+        cv2.imwrite(img_name, frame)
+        frame_num += 1
+        print('saving frame {}')
+
+    h.set_data(frame_rgb)
     plt.draw()
     plt.pause(0.001)
 
